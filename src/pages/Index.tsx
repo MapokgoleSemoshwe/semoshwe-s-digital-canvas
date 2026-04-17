3cefdc1cecd990ba3bbbd106990055409c65d077
semos <semoshwemapokgole.gmail.com>
2026-04-17 23:40:40 +0200

    commit 6

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -4,11 +4,7 @@ import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import SkillsSection from "@/components/SkillsSection";
 import ContactSection from "@/components/ContactSection";
-<<<<<<< HEAD
-//import ChatBotButton from "@/components/ChatBotButton";
-=======
 import ChatBotButton from "@/components/ChatBotButton";
->>>>>>> 82b968d9e7cc0b1ce1ed8a4f907402d8f50a8027
 import Footer from "@/components/Footer";
 
 const Index = () => (
@@ -20,11 +16,7 @@ const Index = () => (
     <SkillsSection />
     <ContactSection />
     <Footer />
-<<<<<<< HEAD
     
-=======
-    <ChatBotButton />
->>>>>>> 82b968d9e7cc0b1ce1ed8a4f907402d8f50a8027
   </div>
 );
 
